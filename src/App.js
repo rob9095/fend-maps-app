cad1bc3ff305c32575855988f494e49ca3c3e2d0
Rob Tucker <robt9095@gmail.com>
2018-10-07 15:14:01 -0700

    moved menuIcon into its own file

--- src/App.js
+++ src/App.js
@@ -4,31 +4,11 @@ import Map from './Map';
 import SpotMenu from './SpotMenu';
 import SpotModal from './SpotModal';
 import { Button, Icon, Input, Spin } from 'antd'
+import MenuIcon from './MenuIcon';
 import {apiCall} from './services';
 import {activeSpots} from './activeSpots'
 
 const Search = Input.Search;
-
-const menuSvg = () => (
-  <svg viewBox="0 0 384.97 384.97" fill="currentColor" width="1em" height="1em">
-    <g>
-    	<g id="Menu">
-    		<path d="M12.03,84.212h360.909c6.641,0,12.03-5.39,12.03-12.03c0-6.641-5.39-12.03-12.03-12.03H12.03
-    			C5.39,60.152,0,65.541,0,72.182C0,78.823,5.39,84.212,12.03,84.212z"/>
-    		<path d="M372.939,180.455H12.03c-6.641,0-12.03,5.39-12.03,12.03s5.39,12.03,12.03,12.03h360.909c6.641,0,12.03-5.39,12.03-12.03
-    			S379.58,180.455,372.939,180.455z"/>
-    		<path d="M372.939,300.758H12.03c-6.641,0-12.03,5.39-12.03,12.03c0,6.641,5.39,12.03,12.03,12.03h360.909
-    			c6.641,0,12.03-5.39,12.03-12.03C384.97,306.147,379.58,300.758,372.939,300.758z"/>
-    	</g>
-    </g>
-  </svg>
-)
-
-const MenuIcon = props => (
-  <span className="custom-icon">
-    <Icon component={menuSvg} {...props} />
-  </span>
-);
 
 class App extends Component {
   state = {
